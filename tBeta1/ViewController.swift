353c24e2b5d14feeb728d5bd7d1f9043b6ad96c3
aaronmiller <aaronmiller822028@gmail.com>
2017-11-20 00:49:23 -0800

    Pushing on xCode is nice

--- tBeta1/ViewController.swift
+++ tBeta1/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+@IBOutlet weak var background: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,3 +28,4 @@ class ViewController: UIViewController {
 
 }
 
+    
