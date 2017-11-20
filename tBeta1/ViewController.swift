75eacbb8c47ce57d97c8a69492b0bc680eec23a5
aaronmiller <aaronmiller822028@gmail.com>
2017-11-20 00:18:35 -0800

    Initial Commit

---
+++ tBeta1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tBeta1
+//
+//  Created by Aaron Miller on 11/20/17.
+//  Copyright © 2017 Aaron Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
