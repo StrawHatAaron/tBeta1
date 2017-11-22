d937ec8c18c73cde4cd691855eb5a31b0ea0db9d
aaronmiller <aaronmiller822028@gmail.com>
2017-11-21 22:25:12 -0800

    Can now properly tell what state the bluetooth of the phone is in.

--- tBeta1/ViewController.swift
+++ tBeta1/ViewController.swift
@@ -9,37 +9,40 @@
 import UIKit
 import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate {
     
-@IBOutlet weak var background: UIImageView!//nothing done with the background pic
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var buttonSearch: UIButton!
+    
+    var managerC:CBCentralManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        //Core BLE
-        //peripherals
-        var peripheralDelegateName: String
-        var peripheralDelegate: CBPeripheralDelegate
-        var peripheral:CBPeripheral
-        //managers
-        var managerCentralDelegate:CBCentralManagerDelegate
-        var managerCentral:CBCentralManager
-        managerCentral = CBCentralManager()
-        //CBUUID array
-        var uuidStr = "16107de1-46b4-4a21-856b-fe1c737da2fe"
-        var uuidData = Data.init(count: 8)
-        var uuid = CBUUID.init(stringID: uuidStr, intID: uuidData)
-        
-        
-        centralManagerDidUpdateState(managerCentral)
-       //LEFT OFF HERE. Can't scan yet anyways so it's a good place to stop
-       // scanForPeripherals(withServices serviceUUIDs: [uuid]?)
+        buttonSearch.layer.cornerRadius = 10
+        buttonSearch.clipsToBounds = true
+        managerC = CBCentralManager(delegate: self, queue: nil)
+        centralManagerDidUpdateState(managerC)
     }
 
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Check the device bluetooth status.
+    func centralManagerDidUpdateState(_ central: CBCentralManager!) {
+        switch (central.state) {
+        case .poweredOff:
+            print("powered off")
+        case .resetting:
+            print("resetting")
+        case .poweredOn:
+            print("powered on")
+            //so if its powered on then we need to scan
+        case .unauthorized:
+            print("unauth")
+        case .unknown:
+            print("unkown state this what we have been getting /:")
+        case .unsupported:
+            print("unsupported")
+        default:
+            print("weird")
+        }
     }
 
     //allows for a 16-bit 32-bit or 128-bit UUID
@@ -52,30 +55,11 @@ class ViewController: UIViewController {
             self.uuidData = intID
         }
     }
-
-    //Check the device bluetooth status.
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        if central.state == .poweredOn {
-            print("Bluetooth is connected")
-        }
-        else if central.state == .resetting{
-            print("Bluetooth is resetting")
-        }
-        else if central.state == .unauthorized{
-            print("Bluetooth is unauthorized")
-        }
-        else if central.state == .unknown{
-            print("Bluetooth is unkown state")//ended in an unknown state. Check Simulator settings
-        }
-        else if central.state == .unsupported{
-            print("Bluetooth is unsupported")
-        }
-        else if central.state == .poweredOff{
-            print("Bluetooth is not Connected.Please Enable it")
-        }
-    }
     
     func scanForPeripherals(withServices serviceUUIDs: [CBUUID]?,options: [String : Any]? = nil){}//is it even doing anything
-}
 
-    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
